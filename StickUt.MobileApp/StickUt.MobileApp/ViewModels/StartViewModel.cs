1b9fddbf827d13ce52e4b912ff941462e97151cb
Andreas Nilsson <andreas@effemcom.com>
2016-06-19 09:36:25 +0200

    x

--- StickUt.MobileApp/StickUt.MobileApp/ViewModels/StartViewModel.cs
+++ StickUt.MobileApp/StickUt.MobileApp/ViewModels/StartViewModel.cs
@@ -31,15 +31,17 @@ namespace StickUt.MobileApp.ViewModels
             {
                 return _buttoncmd ?? (_buttoncmd = new Command(() => 
                 {
-                    _dialog.ShowSpinner(() => { _dialog.HideDialog(); });
-
-                    //using (var scope = App.Container.BeginLifetimeScope())
-                    //{
-                    //    var view = scope.Resolve<MainSettingsView>();
-                    //    Debug.WriteLine(view.GetType().ToString());
-                    //    Device.BeginInvokeOnMainThread(async () => 
-                    //    { await App.Context.Navigation.PushAsync(view); });                        
-                    //}                 
+                    _dialog.Toast("klicka", () => 
+                    {
+                        _dialog.HideDialog();
+                        using (var scope = App.Container.BeginLifetimeScope())
+                        {
+                            var view = scope.Resolve<MainSettingsView>();
+                            Debug.WriteLine(view.GetType().ToString());
+                            Device.BeginInvokeOnMainThread(async () =>
+                            { await App.Context.Navigation.PushAsync(view); });
+                        }                        
+                    });
                 }));
             }
         }
