f2da8416328ff3153a22891a3a6842482448b820
Andreas Nilsson <andreas@effemcom.com>
2016-06-29 09:07:40 +0200

    added untracked files

---
+++ StickUt.Interface/ILocalStorage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StickUt.Interface
+{
+    public interface ILocalStorage
+    {
+        IEnumerable<T> GetItems<T>() where T : class, new();
+        void Insert<T>(T obj) where T : class, new();
+        void Update<T>(T obj) where T : class, new();
+        void Upsert<T>(T obj) where T : class, new();
+        IEnumerable<T> Query<T>(Func<T, bool> query) where T : class, new();
+        void Delete<T>(Guid id) where T : class, new();
+    }
+}
